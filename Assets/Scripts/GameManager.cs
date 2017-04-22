23b22faf12a0dbe3fbd23a44ac99961a99b6dbeb
Rica Feng <rfeng001@ucr.edu>
2017-04-22 03:07:56 -0700

    Game Manager and game manager prefab

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    private GameState _State;
+    public GameState State
+    {
+        get { return _State; }
+        set
+        {
+            if (value == _State)
+                return;
+
+            _State.OnStateExit();
+
+            _State = value;
+
+            _State.OnStateEnter();
+        }
+    }
+
+    private Player PlayerOne;
+    private Player PlayerTwo;
+
+    public Player CurrentPlayer { get; private set; }
+
+    private GameObject UITabReference;
+
+    private void Awake()
+    {
+        CurrentPlayer = PlayerOne;
+    }
+
+    public void SwitchTurn()
+    {
+        if (CurrentPlayer == PlayerOne)
+            CurrentPlayer = PlayerTwo;
+        else
+            CurrentPlayer = PlayerOne;
+    }
+
+
+}

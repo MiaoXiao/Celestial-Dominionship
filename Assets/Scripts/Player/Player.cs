b480492ebcd83d9d0f0aaf09f3e8ef6ecf26b350
nadnbuds <lalam003@ucr.edu>
2017-04-22 01:09:36 -0700

    Player Dummy

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    List<Celest> CurrentHand;
+    List<Celest> Deck;
+    List<Celest> Discard;
+    Grid Field;
+    int Dust;
+    int buysAvailible;
+
+    public delegate void Passives();
+
+    private void Display()
+    {
+
+    }
+
+    public void SetActive()
+    {
+
+    }
+
+    public void Play()
+    {
+
+    }
+
+    public void Buy()
+    {
+
+    }
+
+}

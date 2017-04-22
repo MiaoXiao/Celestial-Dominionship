08a97c6298f8734c9f88ebabbd07f1c7efaa5da4
nadnbuds <lalam003@ucr.edu>
2017-04-22 03:14:08 -0700

    Merge branch 'master' of https://github.com/MiaoXiao/Celestial-Dominionship

---
+++ Assets/Scripts/States/InitGameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitGameState : GameState
+{
+    public override void OnStateEnter()
+    {
+        Player current_player = GameManager.Instance.CurrentPlayer;
+    }
+
+    public override void OnStateExit()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/States/SwitchPlayerState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayerState : GameState
+{
+    public override void OnStateEnter()
+    {
+        
+    }
+
+    public override void OnStateExit()
+    {
+        throw new NotImplementedException();
+    }
+}

14d6b5d737255d6e3950e39122232ef443bc9cc9
Jose Garcia <josegarcia8064@gmail.com>
2017-04-22 17:44:30 -0700

    Added TitleMenu

---
+++ Assets/Scripts/TitleScene/InfoTab.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfoTab : MonoBehaviour {
+
+    public GameObject infotab;
+    public int changeLocation = 672;
+    public float deltaLocation = 0;
+    public int infoStatus = 1;
+
+    public List<string> infolines;
+    public Text infoTextBox;
+    public int textCurrent = -1;
+
+	// Use this for initialization
+	void Start () {
+        infotab = gameObject;
+        ChangeInfoText();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (deltaLocation != 0)
+        {
+            MoveInfoTab();
+        }
+	}
+
+    public void StartInfoTab()
+    {
+        if (infoStatus > 0)
+        {
+            deltaLocation = changeLocation;
+            infoStatus *= -1;
+        }
+        else
+        {
+            deltaLocation = -changeLocation;
+            infoStatus *= -1;
+        }
+    }
+
+    public void MoveInfoTab()
+    {
+        float updatePosition = infoStatus * Time.deltaTime * changeLocation;
+        gameObject.transform.Translate(updatePosition, 0, 0);
+        deltaLocation += updatePosition;
+
+        if (deltaLocation < 0 && infoStatus < 0)
+        {
+            deltaLocation = 0;
+        }
+        else if (deltaLocation > 0 && infoStatus > 0)
+        {
+            deltaLocation = 0;
+        }
+    }
+
+    public void ChangeInfoText()
+    {
+        textCurrent++;
+        if (textCurrent >= infolines.Count)
+        {
+            textCurrent = 0;
+        }
+        infoTextBox.text = infolines[textCurrent];
+    }
+}
---
+++ Assets/Scripts/TitleScene/TitleScene.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScene : MonoBehaviour{
+
+    public string PlayMenu = "PlayMenu";
+    public string InfoMenu = "InformationMenu";
+
+    public void GoToPlay()
+    {
+        SceneManager.LoadScene("Play");
+    }
+
+    public void GoToInfo()
+    {
+        GameObject.Find("InfoTab").GetComponent<InfoTab>().StartInfoTab();
+    }
+
+    public void GoToQuit()
+    {
+        Application.Quit();
+    }
+
+}

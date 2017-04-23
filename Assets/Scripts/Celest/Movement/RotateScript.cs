51ef3d772f2b9c515576d46270ac009b2a115f88
Jose Garcia <josegarcia8064@gmail.com>
2017-04-22 18:59:40 -0700

    Added Movement and Rotate Scritps

---
+++ Assets/Scripts/Celest/Movement/RotateScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+
+    public Transform gameTransform;
+
+    public float speed;
+    public bool isActive;
+    public Vector3 direction;
+
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        gameTransform = gameObject.transform;
+        isActive = false;
+        StartRotation();
+    }
+
+    public void StartRotation()
+    {
+        isActive = true;
+        StartCoroutine("WaitandRotate");
+    }
+
+    IEnumerator WaitandRotate()
+    {
+        while (isActive)
+        {
+            Debug.Log("hi");
+            gameTransform.Rotate(speed * direction * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+}

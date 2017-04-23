d078b07a56833d2cc1ba20acbdcec26a448208c8
Jose Garcia <josegarcia8064@gmail.com>
2017-04-23 02:29:26 -0700

    Made NewsAlert Prettier.  ٩(◕‿◕｡)۶

--- Assets/Scripts/NewsAlerts/NewsAlert.cs
+++ Assets/Scripts/NewsAlerts/NewsAlert.cs
@@ -5,49 +5,54 @@ using UnityEngine.UI;
 
 public class NewsAlert : MonoBehaviour {
 
+    public List<string> Phrases;
+
     [SerializeField]
-    private List<string> Phrases;
+    private List<string> EndPhrases;
 
     [SerializeField]
     private List<string> StartPhrases;
 
-    public Text textbox;
+    public Text lastTextBox;
 
     public Vector3 startLocation;
-    public int speed;
 
-    [SerializeField]
-    float time = 0;
+    public GameObject textBox;
 
     void Start()
     {
-        startLocation = transform.TransformPoint(textbox.transform.localPosition);
-        StartCoroutine("WaitAndMove");
+        startLocation = new Vector3(GetComponent<RectTransform>().rect.x + GetComponent<RectTransform>().rect.width, transform.position.y, transform.position.z);
+        //textBox = 
+        StartCoroutine("WaitAndSpawn");
     }
 
-    void Update()
+    private void CallNewsAlert()
     {
-        time += Time.deltaTime;
-        if (time > 20)
+        GameObject temp = Instantiate(textBox, transform);
+        temp.transform.position = startLocation;
+        temp.GetComponent<Text>().text = Phrases[0];
+        Phrases.RemoveAt(0);
+
+        lastTextBox = temp.GetComponent<Text>();
+    }
+
+
+    IEnumerator WaitAndSpawn()
+    {
+        while (true)
         {
-            time = 0;
-            CallNewsAlert();
+            TestifParsed();
+            yield return null;
         }
     }
 
-    public void CallNewsAlert(/*string planetName, int health*/)
+    public void AddNewsAlert(string planetName = "Earth", int health = 1)
     {
-        textbox.transform.position = startLocation;
-
-        string planetName = "Earth";
-        int health = Random.Range(1, 5);
-
-        Debug.Log("hi");
-
         string population = Random.Range(1, 999).ToString() + " ";
         switch (health)
         {
-            case 1: population += "thousand";
+            case 1:
+                population += "thousand";
                 break;
             case 2:
                 population += "million";
@@ -62,15 +67,49 @@ public class NewsAlert : MonoBehaviour {
                 population += "quadrillion";
                 break;
         }
-        textbox.text = string.Format(StartPhrases[Random.Range(0, StartPhrases.Count)] + Phrases[Random.Range(0, Phrases.Count)], planetName, population);
+        Phrases.Add(string.Format(StartPhrases[Random.Range(0, StartPhrases.Count)] + EndPhrases[Random.Range(0, EndPhrases.Count)], planetName, population) + "     ");
     }
 
-    IEnumerator WaitAndMove()
+    public void AddNewsAlert()
     {
-        while (true)
+        Debug.Log("hi");
+        string planetName = "Earth"; int health = 1;
+        string population = Random.Range(1, 999).ToString() + " ";
+        switch (health)
         {
-            textbox.transform.Translate(speed * Vector3.left * Time.deltaTime);
-            yield return null;
+            case 1:
+                population += "thousand";
+                break;
+            case 2:
+                population += "million";
+                break;
+            case 3:
+                population += "billion";
+                break;
+            case 4:
+                population += "trillion";
+                break;
+            case 5:
+                population += "quadrillion";
+                break;
+        }
+        Phrases.Add(string.Format(StartPhrases[Random.Range(0, StartPhrases.Count)] + EndPhrases[Random.Range(0, EndPhrases.Count)], planetName, population) + "     ");
+    }
+
+    public void AddCustomNewsAlert(string announcement)
+    {
+        Phrases.Add(announcement);
+    }
+
+    private void TestifParsed()
+    {
+        if (Phrases.Count == 0) return;
+        if (lastTextBox == null) CallNewsAlert();
+
+        //Debug.Log((lastTextBox.transform.position.x + lastTextBox.rectTransform.rect.width) + " <? " + GetComponent<RectTransform>().rect.width);
+        if (lastTextBox.transform.position.x + lastTextBox.rectTransform.rect.width < GetComponent<RectTransform>().rect.width)
+        {
+            CallNewsAlert();
         }
     }
 
---
+++ Assets/Scripts/NewsAlerts/TextBoxMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxMovement : MonoBehaviour {
+
+    public int speed;
+    public Vector3 direction;
+
+    private Rect ownRect;
+
+	// Use this for initialization
+	void Start () {
+        ownRect = GetComponent<RectTransform>().rect;
+        ownRect.width = GetComponent<Text>().preferredWidth;
+        StartCoroutine("WaitAndMove");
+    }
+
+    IEnumerator WaitAndMove()
+    {
+        while (true)
+        {
+            transform.Translate(speed * direction * Time.deltaTime);
+            TestifGone();
+            yield return null;
+        }
+    }
+
+    private void TestifGone()
+    {
+        //Debug.Log(ownRect.width + " " + GetComponent<Text>().preferredWidth);
+        //Debug.Log(transform.position.x + " <? " + -ownRect.width);
+        if (transform.position.x < -ownRect.width)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
